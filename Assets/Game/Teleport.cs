cab292b551e62520dc77cfdecf50b1aa031af403
Sami Haapanen <samimha@metropolia.fi>
2017-04-11 19:14:44 +0300

    Added teleport function and better variable names

--- Assets/Game/Teleport.cs
+++ Assets/Game/Teleport.cs
@@ -7,31 +7,32 @@ namespace Game
 {
     public class Teleport : MonoBehaviour
     {
-        GameObject dude;
-        Button tp;
-		Button tp2;
-        Vector2 pos;
+        GameObject player;
+        Button from;
+		Button to;
 
+		void SetTP(string door1, string door2)
+		{
+			from = GameObject.Find(door1).GetComponent<Button>();
+			to = GameObject.Find(door2).GetComponent<Button>();
+			player = GameObject.Find("Player");
+		}
 
         void Start()
         {
-            tp = GameObject.Find("Teleporter").GetComponent<Button>();
-			tp2 = GameObject.Find("Teleporter2").GetComponent<Button>();
-            dude = GameObject.Find("Player");
-            tp.onClick.AddListener(() => Tele());
-			pos = new Vector2(tp2.transform.position.x, -8f);
+			SetTP("Teleporter","Teleporter2");
+			from.onClick.AddListener(() => Tele());
         }
 
-
-        void Update()
-        {
-
-        }
+		void Update()
+		{
+		}
 
         void Tele()
         {
-            dude.transform.position = pos;
-			Camera.main.transform.position = new Vector3(tp2.transform.position.x, 3f, -10f);
+			player.transform.position = new Vector2(to.transform.position.x, -8f);
+			Camera.main.transform.position = new Vector3(to.transform.position.x, 3f, -10f);
         }
+
     }
 }
