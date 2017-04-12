6102c834250185f2ba9730dcaaf98e3b984875d6
Sami Haapanen <samimha@metropolia.fi>
2017-04-12 12:56:40 +0300

    Added more doors

--- Assets/Game/Teleport.cs
+++ Assets/Game/Teleport.cs
@@ -7,23 +7,41 @@ namespace Game
 {
     public class Teleport : MonoBehaviour
     {
-		GameObject player, rws1;     
+		GameObject player, rws1, mh1, mh2, bar1;
+		CameraMovement camera;
 
 		void Start()
 		{
 			player = GameObject.Find("Player");
+			camera = FindObjectOfType<CameraMovement> ();
 			rws1 = (GameObject)Resources.Load ("DoorRWS1", typeof(GameObject));
+			mh1 = (GameObject)Resources.Load ("DoorMainHall1", typeof(GameObject));
+			mh2 = (GameObject)Resources.Load ("DoorMainHall2", typeof(GameObject));
+			bar1 = (GameObject)Resources.Load ("DoorMainHall3", typeof(GameObject));
 		}
 
 		public void Tele(NPC.Collision teleporter)
         {
+			camera.moveRight = false;
+			camera.moveLeft = false;
 			string name = teleporter.GetName ();
-			Debug.Log (name);
 			switch (name)
 			{
 			case "DoorRWS1(Clone)":
 				player.transform.position = new Vector2 (-60f, -8f);
 				Camera.main.transform.position = new Vector3 (-60f, 3f, -10f);
+				break;
+			case "DoorMainHall1(Clone)":
+				player.transform.position = new Vector2 (20f, -8f);
+				Camera.main.transform.position = new Vector3 (20f, 3f, -10f);
+				break;
+			case "DoorMainHall2(Clone)":
+				player.transform.position = new Vector2 (-140f, -8f);
+				Camera.main.transform.position = new Vector3 (-140f, 3f, -10f);
+				break;
+			case "DoorBar1(Clone)":
+				player.transform.position = new Vector2 (-80f, -8f);
+				Camera.main.transform.position = new Vector3 (-80f, 3f, -10f);
 				break;
 			}
         }
