deab7564213947d69a0381cbc6d7c8d6ae5691af
Sami <sami.haapanen@hotmail.com>
2017-04-17 15:04:17 +0300

    Change and comments to enter door
    Enter door takes now players and cameras next location separately so tha camera moves straight to the correct location.

--- Assets/Game/Door.cs
+++ Assets/Game/Door.cs
@@ -24,29 +24,28 @@ namespace Game
 			switch (name)
 			{
 			    case "DoorRWS1(Clone)":
-                    EnterDoor(-60, -105, -68);
+                    EnterDoor(-60, -105, -68, -68);
 				    break;
 			    case "DoorMainHall1(Clone)":
-                    EnterDoor(20, 0, 140);
+                    EnterDoor(20, 0, 140, 20);
 				    break;
 			    case "DoorMainHall2(Clone)":
-                    EnterDoor(-140, -182, -161);
+                    EnterDoor(-140, -182, -161, -161);
 				    break;
 			    case "DoorBar1(Clone)":
-                    EnterDoor(-80, -105, -68);
+                    EnterDoor(-80, -105, -68, -80);
 				    break;
                 case "DoorMainHall3(Clone)":
-                    EnterDoor(-225, -250, -230);
+                    EnterDoor(-225, -250, -240, -240);
                     break;
                 case "DoorMetroHelsinki1(Clone)":
-                    EnterDoor(-125, -105, -68);
+                    EnterDoor(-125, -105, -68, -105);
                     break;
                 case "MetroHelsinki(Clone)":
                     if(player.money > 4.9)
                     {
-                        EnterDoor(-300, -320, -305);
+                        EnterDoor(-300, -320, -315, -315);
                         player.useMoney(-5);
-                        //spend time
                         events.ChangeTime(10);
                     }
                     else
@@ -57,9 +56,8 @@ namespace Game
                 case "MetroSornainen(Clone)":
                     if (player.money > 4.9)
                     {
-                        EnterDoor(-260, -250, -230);
+                        EnterDoor(-260, -250, -240, -250);
                         player.useMoney(-5);
-                        //spend time
                         events.ChangeTime(10);
                     }
                     else
@@ -69,11 +67,17 @@ namespace Game
                     break;
             }
         }
-
-        private void EnterDoor(float xLocation, float xMin, float xMax)
+        /// <summary>
+        /// Transports player and camera to the next area and sets the cameras borders in that area.
+        /// </summary>
+        /// <param name="xLocationPlayer"> Player moves to this location </param>
+        /// <param name="xMin"> Cameras left border in next area </param>
+        /// <param name="xMax"> Cameras right border in next area </param>
+        /// <param name="xLocationCam"> Camera moves to this locatioin </param>
+        private void EnterDoor(float xLocationPlayer, float xMin, float xMax, float xLocationCam)
         {
-            character.transform.position = new Vector2(xLocation, -8f);
-            Camera.main.transform.position = new Vector3(xLocation, 3f, -10f);
+            character.transform.position = new Vector2(xLocationPlayer, -8f);
+            Camera.main.transform.position = new Vector3(xLocationCam, 3f, -10f);
             limits.xMin = xMin;
             limits.xMax = xMax;
         }
